09c2faecffcdb66b183fffb23fb4deeb8934a11e
Administrador <Administrador@javi>
2015-03-30 20:34:27 +0200

    Se añaden beans y actions para modificar/añadir conocimiento, educacion y experiencia. Se añade tambien form para busqueda

---
+++ src/java/com/myapp/struts/Controlador/Forms/PerfilCompletoForm.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.myapp.struts.Controlador.Forms;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts.action.ActionErrors;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionMessage;
+
+/**
+ *
+ * @author Administrador
+ */
+public class PerfilCompletoForm extends org.apache.struts.action.ActionForm {
+    
+    private String nombre, apellidos, nomusuario, email, foto, perfil, pdf;
+
+    /**
+     *
+     */
+    public PerfilCompletoForm() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * This is the action called from the Struts framework.
+     *
+     * @param mapping The ActionMapping used to select this instance.
+     * @param request The HTTP Request we are processing.
+     * @return
+     */
+    public ActionErrors validate(ActionMapping mapping, HttpServletRequest request) {
+        ActionErrors errors = new ActionErrors();
+        
+        return errors;
+    }
+}
